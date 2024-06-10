9e5c07250fd25825393161d12450f6858996114e
jonathan tandazo <jv.tandazom@uea.edu.ec>
2024-06-09 22:42:48 -0500

    POSITIVO

---
+++ TECNICAS DE PROGRAMACION.py
@@ -0,0 +1,5 @@
+#Repositorio de código fuente utilizado en la Asignatura Programación Orientada a Objetos
+# #Institución: Universidad Estatal Amazónica #Carrera: Ingeniería en Tecnologías de la Información y Comunicación
+# #Docente: Ing. Edwin Gustavo Fernández Sánchez, Mgs.
+# #Nombre: Jonathan Tandazo
+#BIENVENIDOS A MI REPOSITORIO
